075f705e6b269cf2ecc734b056ec8927b22e8516
Jerry Wang <jianjunw@cn.ibm.com>
2021-03-31 09:50:51 +0800

    update

--- lua/jw/lsp_config.lua
+++ lua/jw/lsp_config.lua
@@ -91,6 +91,7 @@ end
 -- npm install -g vscode-json-languageserver
 -- npm install -g vim-language-server
 -- npm install -g sql-language-server
+-- npm install -g diagnostic-languageserver
 --
 --
 -- yarn global add typescript typescript-language-server vscode-html-languageserver-bin vls vscode-css-languageserver-bin bash-language-server vscode-json-languageserver vim-language-server sql-language-server
@@ -202,6 +203,51 @@ lspconfig.jedi_language_server.setup {
   capabilities = lsp_status.capabilities,
 }
 -- lspconfig.yamlls.setup{on_attach=on_attach_vim}
+
+lspconfig.diagnosticls.setup{
+  on_attach=on_attach_vim,
+  capabilities = lsp_status.capabilities,
+    filetypes = {"javascript", "typescript"},
+    root_dir = function(fname)
+      return lspconfig.util.root_pattern("tsconfig.json")(fname) or lspconfig.util.root_pattern(".eslintrc.js")(fname);
+    end,
+    init_options = {
+      linters = {
+        eslint = {
+          command = "./node_modules/.bin/eslint",
+          rootPatterns = {".eslintrc.js", ".git"},
+          debounce = 100,
+          args = {
+            "--stdin",
+            "--stdin-filename",
+            "%filepath",
+            "--format",
+            "json"
+          },
+          sourceName = "eslint",
+          parseJson = {
+            errorsRoot = "[0].messages",
+            line = "line",
+            column = "column",
+            endLine = "endLine",
+            endColumn = "endColumn",
+            message = "[eslint] ${message} [${ruleId}]",
+            security = "severity"
+          },
+          securities = {
+            [2] = "error",
+            [1] = "warning"
+          }
+        },
+      },
+      filetypes = {
+        javascript = "eslint",
+        typescript = "eslint"
+      }
+    }
+  }
+
+
 
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
